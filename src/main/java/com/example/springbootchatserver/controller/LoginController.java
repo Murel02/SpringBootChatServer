8bd899152a9511ebfccc58cea1d308f934497669
Murel02 <olimurel@gmail.com>
2024-09-23 11:43:27 +0200

    Changes by robta

--- src/main/java/com/example/springbootchatserver/controller/LoginController.java
+++ src/main/java/com/example/springbootchatserver/controller/LoginController.java
@@ -3,17 +3,19 @@ package com.example.springbootchatserver.controller;
 import com.example.springbootchatserver.service.UserService;
 import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+@Controller
 public class LoginController {
 
     @Autowired
     private UserService userService;
 
-    @GetMapping("login")
+    @GetMapping("/login")
     public String loginPage(){
         return "login";
     }
